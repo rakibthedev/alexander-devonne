f50c48f4696ccd92c67435aa3ac715bb2f0d6a4b
abdur-rakib <abdurrakib4896@gmail.com>
2024-11-28 14:15:20 +0600

    70% may done

--- pages/api/logout/logout.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-    // Set the `authToken` cookie to expire in the past
-    res.setHeader('Set-Cookie', 'authToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict');
-    res.status(200).json({ message: 'Logged out successfully' });
-}
