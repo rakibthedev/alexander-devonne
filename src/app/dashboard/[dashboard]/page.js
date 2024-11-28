8d4b40a936afd74fd83273c68713dc7324d6d81d
abdur-rakib <abdurrakib4896@gmail.com>
2024-11-28 14:29:13 +0600

    70% may done

--- src/app/dashboard/[dashboard]/page.js
+++ src/app/dashboard/[dashboard]/page.js
@@ -6,14 +6,15 @@ import Profile from '@/app/components/account/dashboard/Profile'
 import Orders from '@/app/components/account/dashboard/Orders'
 import Addresses from '@/app/components/account/dashboard/Addresses'
 import PaymentMethods from './../../components/account/dashboard/PaymentMethods';
+import { usePathname } from 'next/navigation';
 
 export default function page() {
+  const currentPath = usePathname();
   
   return (
     <div className='pt-5 pb-32 px-3 lg:px-5 bg-[#ddd7d6] min-h-[500px]'>
-      {/* <div className="flex flex-col lg:flex-row gap-5 lg:gap-20">
+      <div className="flex flex-col lg:flex-row gap-5 lg:gap-20">
         <div className="flex-[100%] lg:flex-[60%]">
-          
             <Profile />
           
           
@@ -28,7 +29,7 @@ export default function page() {
         <div className="flex-[100%] lg:flex-[40%] flex mt-5 justify-end">
           <DashboardMenu />
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
