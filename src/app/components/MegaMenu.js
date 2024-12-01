daec71d246b94d85bc57444e8039eed7a9b54846
abdur-rakib <abdurrakib4896@gmail.com>
2024-12-01 17:02:28 +0600

    finalizing project

--- src/app/components/MegaMenu.js
+++ src/app/components/MegaMenu.js
@@ -4,12 +4,12 @@ export default function MegaMenu({ subMenu = [] }) { // Provide a default value
   return (
     <div className="menu__container absolute top-[34px] left-0 w-full opacity-0 invisible group-hover:opacity-100 group-hover:visible bg-black/70 min-h-screen hover:invisible hover:opacity-0">
       <div className='menu__wrapper bg-white flex justify-center gap-5 pt-7 pb-7 flex-wrap w-full'>
-        {subMenu.map((item, index) => (
+        {subMenu?.map((item, index) => (
           <div key={index} className='flex flex-col gap-2 lg:min-w-[200px] delay-200'>
             <Link className='font-medium hover:underline uppercase font-ibmPlexMedium text-[13px] mb-2' href={item.sub_menu_link}>
               {item.sub_menu_title}
             </Link>
-            {item.sub_menu_items.map((sub_menu_item, id) => (
+            {item?.sub_menu_items?.map((sub_menu_item, id) => (
               <Link className='hover:underline capitalize text-xs' key={id} href={sub_menu_item.item_link}>
                 {sub_menu_item.item_title}
               </Link>
--- src/app/constants/megaMenu.js
+++ src/app/constants/megaMenu.js
@@ -1,150 +1,296 @@
 export const megaMenuItems = [
-    {"menu":{
+    {menu: {
         "menu_title": "Men",
         "menu_link": "/products/men",
         "sub_menu": [
-            {
-                "sub_menu_title": "Clothing",
-                "sub_menu_link": "/products/clothing",
-                "sub_menu_items": [
-                    { "item_title": "T-shirt", "item_link": "/products/tshirt" },
-                    { "item_title": "Leather", "item_link": "/products/leather" },
-                    { "item_title": "Coats and Jackets", "item_link": "/products/coats-and-jackets" },
-                    { "item_title": "Denim", "item_link": "/products/denim" },
-                    { "item_title": "Pants", "item_link": "/products/pants" }
-                ]
-            },
-            {
-                "sub_menu_title": "Shoes",
-                "sub_menu_link": "/products/shoes",
-                "sub_menu_items": [
-                    { "item_title": "Sneakers", "item_link": "/products/sneakers" },
-                    { "item_title": "Out of office", "item_link": "/products/out-of-office" },
-                    { "item_title": "Be Right Back", "item_link": "/products/be-right-back" }
-                ]
-            },
-            {
-                "sub_menu_title": "Bags",
-                "sub_menu_link": "/products/bags",
-                "sub_menu_items": [
-                    { "item_title": "Backpacks", "item_link": "/products/backpacks" },
-                    { "item_title": "Crossbody bags", "item_link": "/products/crossbody-bags" },
-                    { "item_title": "Tote bags", "item_link": "/products/tote-bags" },
-                    { "item_title": "Waist bags and clutches", "item_link": "/products/waist-bags-and-clutches" }
-                ]
-            },
-            {
-                "sub_menu_title": "Accessories",
-                "sub_menu_link": "/products/accessories",
-                "sub_menu_items": [
-                    { "item_title": "Hats and scarves", "item_link": "/products/hats-and-scarves" },
-                    { "item_title": "Wallets and cardholders", "item_link": "/products/wallets-and-cardholders" },
-                    { "item_title": "Belts", "item_link": "/products/belts" },
-                    { "item_title": "Other accessories", "item_link": "/products/other-accessories" }
-                ]
-            }
+          {
+            "sub_menu_title": "Clothing",
+            "sub_menu_link": "/products/clothing",
+            "sub_menu_items": [
+              {
+                "item_title": "T-shirt",
+                "item_link": "/products/t-shirt"
+              },
+              {
+                "item_title": "Leather",
+                "item_link": "/products/leather"
+              },
+              {
+                "item_title": "Coat and Jacket",
+                "item_link": "/products/coat-and-jacket"
+              },
+              {
+                "item_title": "Denim",
+                "item_link": "/products/denim"
+              },
+              {
+                "item_title": "Pants",
+                "item_link": "/products/pants"
+              }
+            ]
+          },
+          {
+            "sub_menu_title": "Shoes",
+            "sub_menu_link": "/products/shoes",
+            "sub_menu_items": [
+              {
+                "item_title": "Sneaker",
+                "item_link": "/products/sneaker"
+              },
+              {
+                "item_title": "Out of office",
+                "item_link": "/products/out-of-office"
+              },
+              {
+                "item_title": "Be Right Back",
+                "item_link": "/products/be-right-back"
+              }
+            ]
+          },
+          {
+            "sub_menu_title": "Bags",
+            "sub_menu_link": "/products/bags",
+            "sub_menu_items": [
+              {
+                "item_title": "Backpacks",
+                "item_link": "/products/backpacks"
+              },
+              {
+                "item_title": "Crossbody bags",
+                "item_link": "/products/crossbody-bags"
+              },
+              {
+                "item_title": "Tote bags",
+                "item_link": "/products/tote-bags"
+              },
+              {
+                "item_title": "Waist bags and clutches",
+                "item_link": "/products/waist-bags-and-clutches"
+              }
+            ]
+          },
+          {
+            "sub_menu_title": "Accessories",
+            "sub_menu_link": "/products/accessories",
+            "sub_menu_items": [
+              {
+                "item_title": "Hats and scarves",
+                "item_link": "/products/hats-and-scarves"
+              },
+              {
+                "item_title": "Wallets and cardholders",
+                "item_link": "/products/wallets-and-cardholders"
+              },
+              {
+                "item_title": "Belts",
+                "item_link": "/products/belts"
+              },
+              {
+                "item_title": "Other accessories",
+                "item_link": "/products/other-accessories"
+              }
+            ]
+          }
         ]
-    }},
-    {"menu":{
+      }
+    },
+    {menu: {
         "menu_title": "Women",
         "menu_link": "/products/women",
         "sub_menu": [
-            {
-                "sub_menu_title": "Clothing",
-                "sub_menu_link": "/products/clothing",
-                "sub_menu_items": [
-                    { "item_title": "T-shirt & Tops", "item_link": "/products/tshirt-and-tops" },
-                    { "item_title": "Leather", "item_link": "/products/leather" },
-                    { "item_title": "Coats and Jackets", "item_link": "/products/coats-and-jackets" },
-                    { "item_title": "Denim", "item_link": "/products/denim" },
-                    { "item_title": "Pants", "item_link": "/products/pants" }
-                ]
-            },
-            {
-                "sub_menu_title": "Shoes",
-                "sub_menu_link": "/products/shoes",
-                "sub_menu_items": [
-                    { "item_title": "Sneakers", "item_link": "/products/sneakers" },
-                    { "item_title": "Out of office", "item_link": "/products/out-of-office" },
-                    { "item_title": "Be Right Back", "item_link": "/products/be-right-back" }
-                ]
-            },
-            {
-                "sub_menu_title": "Activewear",
-                "sub_menu_link": "/products/activewear",
-                "sub_menu_items": [
-                    { "item_title": "Tops and bras", "item_link": "/products/tops-and-bras" },
-                    { "item_title": "Leggings", "item_link": "/products/leggings" },
-                    { "item_title": "Be Right Back", "item_link": "/products/be-right-back" }
-                ]
-            },
-            {
-                "sub_menu_title": "Bags",
-                "sub_menu_link": "/products/bags",
-                "sub_menu_items": [
-                    { "item_title": "Shoulder Bags", "item_link": "/products/shoulder-bags" },
-                    { "item_title": "Top handle bags", "item_link": "/products/top-handle-bags" },
-                    { "item_title": "Tote bags", "item_link": "/products/tote-bags" },
-                    { "item_title": "Mini bags", "item_link": "/products/mini-bags" }
-                ]
-            },
-            {
-                "sub_menu_title": "Accessories",
-                "sub_menu_link": "/products/accessories",
-                "sub_menu_items": [
-                    { "item_title": "Wallet and cardholders", "item_link": "/products/wallet-and-cardholders" },
-                    { "item_title": "Hats and scarves", "item_link": "/products/hats-and-scarves" },
-                    { "item_title": "Belts", "item_link": "/products/belts" },
-                    { "item_title": "Other accessories", "item_link": "/products/other-accessories" }
-                ]
-            }
+          {
+            "sub_menu_title": "Clothing",
+            "sub_menu_link": "/products/clothing",
+            "sub_menu_items": [
+              {
+                "item_title": "T-shirt & Tops",
+                "item_link": "/products/tshirt-and-tops"
+              },
+              {
+                "item_title": "Leather",
+                "item_link": "/products/leather"
+              },
+              {
+                "item_title": "Coats and Jackets",
+                "item_link": "/products/coats-and-jackets"
+              },
+              {
+                "item_title": "Denim",
+                "item_link": "/products/denim"
+              },
+              {
+                "item_title": "Pants",
+                "item_link": "/products/pants"
+              }
+            ]
+          },
+          {
+            "sub_menu_title": "Shoes",
+            "sub_menu_link": "/products/shoes",
+            "sub_menu_items": [
+              {
+                "item_title": "Sneakers",
+                "item_link": "\"/products/sneakers"
+              },
+              {
+                "item_title": "Out of office",
+                "item_link": "/products/out-of-office"
+              },
+              {
+                "item_title": "Be Right Back",
+                "item_link": "/products/be-right-back"
+              }
+            ]
+          },
+          {
+            "sub_menu_title": "Activewear",
+            "sub_menu_link": "/products/activewear\"",
+            "sub_menu_items": [
+              {
+                "item_title": "Tops and bras",
+                "item_link": "/products/tops-and-bras"
+              },
+              {
+                "item_title": "Leggings",
+                "item_link": "/products/leggings"
+              },
+              {
+                "item_title": "Be Right Back",
+                "item_link": "/products/be-right-back"
+              }
+            ]
+          },
+          {
+            "sub_menu_title": "Bags",
+            "sub_menu_link": "/products/bags",
+            "sub_menu_items": [
+              {
+                "item_title": "Shoulder Bags",
+                "item_link": "/products/shoulder-bags"
+              },
+              {
+                "item_title": "Top handle bags",
+                "item_link": "/products/top-handle-bags"
+              },
+              {
+                "item_title": "Tote bags",
+                "item_link": "/products/tote-bags"
+              },
+              {
+                "item_title": "Mini bags",
+                "item_link": "/products/mini-bags"
+              }
+            ]
+          },
+          {
+            "sub_menu_title": "Accessories",
+            "sub_menu_link": "/products/accessories",
+            "sub_menu_items": [
+              {
+                "item_title": "Wallet and cardholders",
+                "item_link": "/products/wallet-and-cardholders"
+              },
+              {
+                "item_title": "Hats and scarves",
+                "item_link": "/products/hats-and-scarves"
+              },
+              {
+                "item_title": "Belts",
+                "item_link": "/products/belts"
+              },
+              {
+                "item_title": "Other accessories",
+                "item_link": "/products/other-accessories"
+              }
+            ]
+          }
         ]
-    }},
-    {"menu":{
-        "menu_title": "Kids",
-        "menu_link": "/products/kids",
-        "sub_menu": [
-            {
+      }
+    },
+    {
+        menu: {
+            "menu_title": "Kids",
+            "menu_link": "/products/kids",
+            "sub_menu": [
+              {
                 "sub_menu_title": "Boys",
                 "sub_menu_link": "/products/boys",
                 "sub_menu_items": [
-                    { "item_title": "Boy Clothing", "item_link": "/products/boy-clothing" },
-                    { "item_title": "Boy Shoes", "item_link": "/products/boy-shoes" },
-                    { "item_title": "Boy Accessories", "item_link": "/products/boy-accessories" }
+                  {
+                    "item_title": "Boy Clothing",
+                    "item_link": "/products/boy-clothing"
+                  },
+                  {
+                    "item_title": "Boy Shoes",
+                    "item_link": "/products/boy-shoes"
+                  },
+                  {
+                    "item_title": "Boy Accessories",
+                    "item_link": "/products/boy-accessories"
+                  }
                 ]
-            },
-            {
+              },
+              {
                 "sub_menu_title": "Girls",
                 "sub_menu_link": "/products/girls",
                 "sub_menu_items": [
-                    { "item_title": "Girl Clothing", "item_link": "/products/girl-clothing" },
-                    { "item_title": "Girl Shoes", "item_link": "/products/girl-shoes" },
-                    { "item_title": "Girl Accessories", "item_link": "/products/girl-accessories" }
+                  {
+                    "item_title": "Girl Clothing",
+                    "item_link": "/products/girl-clothing"
+                  },
+                  {
+                    "item_title": "Girl Shoes",
+                    "item_link": "/products/girl-shoes"
+                  },
+                  {
+                    "item_title": "Girl Accessories",
+                    "item_link": "/products/girl-accessories"
+                  }
                 ]
-            },
-            {
+              },
+              {
                 "sub_menu_title": "Baby",
                 "sub_menu_link": "/products/baby",
-                "sub_menu_items": []
-            }
-        ]
-    }},
-    {"menu":{
-        "menu_title": "Homeware",
-        "menu_link": "/products/homeware",
+                "sub_menu_items": null
+              }
+            ]
+          }
+    },
+    {menu: {
+        "menu_title": "Gift Items",
+        "menu_link": "/products/gift-items",
         "sub_menu": [
-            {
-                "sub_menu_title": "Homeware",
-                "sub_menu_link": "/products/homeware",
-                "sub_menu_items": [
-                    { "item_title": "Bath", "item_link": "/products/bath" },
-                    { "item_title": "Bedding", "item_link": "/products/bedding" },
-                    { "item_title": "Decorative Accessories", "item_link": "/products/decorative-accessories" },
-                    { "item_title": "Doormats & Rugs", "item_link": "/products/doormats-and-rugs" },
-                    { "item_title": "Soft Furnishings", "item_link": "/products/soft-furnishings" },
-                    { "item_title": "Tableware", "item_link": "/products/tableware" }
-                ]
-            }
+          {
+            "sub_menu_title": "Gift Items",
+            "sub_menu_link": "/products/homeware",
+            "sub_menu_items": [
+              {
+                "item_title": "Bath",
+                "item_link": "/products/bath"
+              },
+              {
+                "item_title": "Bedding",
+                "item_link": "/products/bedding"
+              },
+              {
+                "item_title": "Decorative Accessories",
+                "item_link": "/products/decorative-accessories"
+              },
+              {
+                "item_title": "Doormats & Rugs",
+                "item_link": "/products/doormats-and-rugs"
+              },
+              {
+                "item_title": "Soft Furnishings",
+                "item_link": "/products/soft-furnishings"
+              },
+              {
+                "item_title": "Tableware",
+                "item_link": "/products/tableware"
+              }
+            ]
+          }
         ]
-    }}
+      }
+    }
 ];
